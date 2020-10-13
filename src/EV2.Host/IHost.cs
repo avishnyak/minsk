814e61ea1e56aa2b113beb775c3cb4e38a7e2eba
Anton Vishnyak <anton.vishnyak@gmail.com>
2020-10-12 20:48:06 -0700

    Host compiler in console using IHost

--- src/EV2.Host/IHost.cs
+++ src/EV2.Host/IHost.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace EV2.Host
 {
+    /// <summary>
+    /// A common interface that allows the compiler to communicate to the host.
+    ///
+    /// All methods must be implemented.
+    /// </summary>
     public interface IHost : IDisposable
     {
         /// <Summary>Request the host to restart the compiler service.</Summary>
